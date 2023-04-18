97c385b60526262156741de3884c7bef0d7bc2d1
amrit <amritsingh.d12@gmail.com>
2023-04-18 21:10:09 +0530

    room started - list added - start - Apr 18

--- app/src/main/java/com/example/inventory/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/home/HomeViewModel.kt
@@ -17,19 +17,37 @@
 package com.example.inventory.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.inventory.data.Item
+import com.example.inventory.data.ItemsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 /**
  * View Model to retrieve all items in the Room database.
  */
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val itemsRepository: ItemsRepository) : ViewModel() {
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    val homeUiState: StateFlow<HomeUiState> =
+        itemsRepository
+            .getAllItemsStream()
+            .map { HomeUiState(it) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed((TIMEOUT_MILLIS)),
+                initialValue = HomeUiState()
+            )
 }
 
 /**
  * Ui State for HomeScreen
  */
-data class HomeUiState(val itemList: List<Item> = listOf())
+data class HomeUiState(
+    val itemList: List<Item> = listOf()
+)
